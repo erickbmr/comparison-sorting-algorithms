e74be6875d1e3f480ca7d2fb6ac4fb99906ce522
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-09-24 19:13:18 -0300

    Test: BubbleSort

---
+++ src/test/java/src/BubbleSortTest.java
@@ -0,0 +1,22 @@
+package src;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BubbleSortTest{
+	
+	@Test
+	public void testbubble(){
+		int[] array = {1, 0 , 10, 15, 3, 5, 8};
+		BubbleSort bubble = new BubbleSort();
+		bubble.bubble(array, 7);
+
+		assertEquals(0, array[0]);
+		assertEquals(1, array[1]);
+		assertEquals(3, array[2]);
+		assertEquals(5, array[3]);
+		assertEquals(8, array[4]);
+		assertEquals(10, array[5]);
+		assertEquals(15, array[6]);
+	}
+}
