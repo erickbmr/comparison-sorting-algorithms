b7431cf0ff47ec68836bc58959e920de5bbbc5b5
Leonardo Henrique Guimarães <leonardo.guimaraes20@icloud.com>
2020-09-25 10:12:05 -0300

    Fixing BubbleSort test

--- src/test/java/src/BubbleSortTest.java
+++ src/test/java/src/BubbleSortTest.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BubbleSortTest{
-	
+
 	@Test
 	public void testbubble(){
-		int[] array = {1, 0 , 10, 15, 3, 5, 8};
+		int[] array = {-1, 0 , 10, 15, 3, 5, 8};
 		BubbleSort bubble = new BubbleSort();
-		bubble.bubble(array, 7);
+		array = bubble.bubble(array);
 
-		assertEquals(0, array[0]);
-		assertEquals(1, array[1]);
+		assertEquals(-1, array[0]);
+		assertEquals(0, array[1]);
 		assertEquals(3, array[2]);
 		assertEquals(5, array[3]);
 		assertEquals(8, array[4]);
